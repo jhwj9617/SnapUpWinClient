f4e7e2701090fddd15e052810bc06be58e168e83
Joon Ho <jhwj9617@live.com>
2016-03-02 22:56:03 -0800

    button downloads image

--- SnapUpWinClient/MainWindow.xaml.cs
+++ SnapUpWinClient/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
-﻿using Microsoft.AspNet.SignalR.Client;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.AspNet.SignalR.Client;
 
 namespace SnapUpWinClient
 {
@@ -24,6 +15,9 @@ namespace SnapUpWinClient
     {
         public MainWindow()
         {
+            // PCId = 1
+            // Code = 1234abcd
+
             var querystringData = new Dictionary<string, string>();
             querystringData.Add("PCId", "1"); // PCId is "1"
             var hubConnection = new HubConnection("http://localhost/MVCWebApp/", querystringData);
@@ -44,7 +38,21 @@ namespace SnapUpWinClient
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
+            var assetURL = string.Empty;
+            using (var webClient = new WebClient())
+            {
+                assetURL = webClient.DownloadString("http://localhost/MVCWebApp/Buses/QueryBus?code=1234abcd");
+            }
+            Debug.Write("Received URL: " + assetURL + "\n");
+            string locationMyPictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            string downloadDestination = Path.Combine(locationMyPictures, "SnapUp");
+            string localFilename = Path.Combine(locationMyPictures, @"SnapUp\newFile.jpg");
+            new FileInfo(localFilename).Directory.Create();
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(assetURL, localFilename);
+            }
+            Process.Start(downloadDestination);
         }
     }
 }
