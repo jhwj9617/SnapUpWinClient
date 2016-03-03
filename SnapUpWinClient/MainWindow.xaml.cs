792e3ce8cf6e951ae0dbfa6c4c61060244991ac1
Joon Ho <jhwj9617@live.com>
2016-03-02 21:41:01 -0800

    init

---
+++ SnapUpWinClient/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnapUpWinClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var querystringData = new Dictionary<string, string>();
+            querystringData.Add("PCId", "1"); // PCId is "1"
+            var hubConnection = new HubConnection("http://localhost/MVCWebApp/", querystringData);
+            var hubProxy = hubConnection.CreateHubProxy("SnapUpServer");
+            hubConnection.Start().ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Debug.Write("There was an error opening the connection\n");
+                }
+                else {
+                    Debug.Write("Connected to " + "http://localhost/MVCWebApp/" + "\n");
+                }
+            }).Wait();
+
+
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
